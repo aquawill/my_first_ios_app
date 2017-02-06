fcad0769724c90cdc8f71483999d79d6291c2ec9
aquawill <aquawill@msn.com>
2017-02-06 19:34:00 +0800

    Initial Commit

---
+++ swift class 1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swift class 1
+//
+//  Created by Wu, Guan-Ling on 06/02/2017.
+//  Copyright © 2017 Wu, Guan-Ling. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
